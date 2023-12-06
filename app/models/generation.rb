e13d9d743e62b7c031130474e8a83ec00a9ff715
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-12-06 08:57:37 -0500

    rubocop

---
+++ app/models/generation.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Generation < ApplicationRecord
+  belongs_to :project
+end
---
+++ spec/factories/generations.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :generation do
+    iteration { 1 }
+    project { nil }
+  end
+end
