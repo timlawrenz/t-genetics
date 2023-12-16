054b964a307995dbd1c3941ca6f4e59a29a81fd9
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-12-16 11:58:45 -0500

    cleanup

--- app/models/concerns/inheritable.rb
+++ app/models/concerns/inheritable.rb
@@ -2,6 +2,6 @@ module Inheritable
   extend ActiveSupport::Concern
 
   included do
-    has_one :allele, as: :allelable, touch: true, dependent: :detroy
+    has_one :allele, as: :allelable, touch: true, dependent: :destroy
   end
 end
--- spec/factories/organisms.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :organism do
-    name { 'MyString' }
-  end
-end
