4768cc9c26ab11c5224b771cc0d3b883ce51db0c
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-12-17 17:20:25 -0500

    views

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,4 @@
 //= link_tree ../builds
+//= link tailwind.css
 //= link application.css
+//= link application.tailwind.css
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,5 @@ Rails.application.routes.draw do
   resources :chromosomes
   resources :alleles
 
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root 'chromosomes#index'
 end
