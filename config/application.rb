a35d17a5291c3ac546e1443395cfc260c15661d3
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2023-12-19 18:07:28 -0500

    more

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module TGenetics
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.assets.css_compressor = nil
 
     # Configuration for the application, engines, and railties goes here.
     #
