22638fc45cefd095a2c4e2fe66a904f35a3e57b7
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2024-01-06 15:02:42 -0500

    rubocops

--- app/commands/application_command.rb
+++ app/commands/application_command.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ApplicationCommand
   include T::Command
   include T::Contract
--- app/commands/organisms/set_value.rb
+++ app/commands/organisms/set_value.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module Organisms
   class SetValue < ApplicationCommand
     requires :organism
