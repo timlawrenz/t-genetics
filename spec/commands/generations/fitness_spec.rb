24e85ab8cfc7e0c5e8118a580fe357298b8a759e
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2024-01-12 10:05:07 -0500

    specs

--- spec/commands/generations/fitness_spec.rb
+++ spec/commands/generations/fitness_spec.rb
@@ -45,4 +45,46 @@ RSpec.describe Generations::Fitness do
       expect(call.average_fitness).to be_nil
     end
   end
+
+  context 'when all organisms have fitness' do
+    let(:generation) { FactoryBot.create(:generation) }
+    let(:parameters) { { generation: } }
+
+    before do
+      FactoryBot.create(:organism, generation:, fitness: 100)
+      FactoryBot.create(:organism, generation:, fitness: 50)
+      FactoryBot.create(:organism, generation:, fitness: 30)
+    end
+
+    it { is_expected.to be_success }
+
+    it 'considers all organisms and returns 180 as total_fitness' do
+      expect(call.total_fitness).to eq(180)
+    end
+
+    it 'considers all organisms and returns 60 as average_fitness' do
+      expect(call.average_fitness).to eq(60)
+    end
+  end
+
+  context 'when some organisms have fitness' do
+    let(:generation) { FactoryBot.create(:generation) }
+    let(:parameters) { { generation: } }
+
+    before do
+      FactoryBot.create(:organism, generation:, fitness: 100)
+      FactoryBot.create(:organism, generation:, fitness: 50)
+      FactoryBot.create(:organism, generation:, fitness: nil)
+    end
+
+    it { is_expected.to be_success }
+
+    it 'considers organisms with fitness and returns 150 as total_fitness' do
+      expect(call.total_fitness).to eq(150)
+    end
+
+    it 'considers organisms with fitness and returns 75 as average_fitness' do
+      expect(call.average_fitness).to eq(75)
+    end
+  end
 end
