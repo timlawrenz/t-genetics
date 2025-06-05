175d2463e873e7369397c7f12f798aea8cce092b
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-06-05 14:08:33 -0400

    refactor: Improve organism commands clarity and validation

--- app/commands/organisms/clone.rb
+++ app/commands/organisms/clone.rb
@@ -3,12 +3,13 @@
 module Organisms
   class Clone < GLCommand::Callable
     requires organism: Organism
+    returns dolly_clone: Organism
 
     def call
       create_result = Organisms::Create.call(generation: organism.generation)
-      context.clone = create_result.organism
+      context.dolly_clone = create_result.organism
       context.organism.values.each do |value| # rubocop:disable Style/HashEachMethods
-        context.clone.set_value(value.name, value.data)
+        context.dolly_clone.set_value(value.name, value.data)
       end
     end
   end
--- app/commands/organisms/procreate.rb
+++ app/commands/organisms/procreate.rb
@@ -3,10 +3,13 @@
 module Organisms
   class Procreate < GLCommand::Callable
     requires :parents
+    returns :children
 
     def call
+      errors.add(:parents, 'must be an array of two parents') unless parents.size == 2
+
       context.children = parents.map(&:dolly_clone)
-      Organisms::Crossover.call(organisms: context.children)
+      Organisms::Crossover.call!(organisms: context.children)
       context.children.map(&:mutate!)
     end
   end
