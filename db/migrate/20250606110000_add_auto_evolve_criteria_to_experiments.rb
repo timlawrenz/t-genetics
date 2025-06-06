2e0beeaa98148a1fd83fc43827ef6a0caefc4fde
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-06-06 13:42:48 -0400

    chore: Remove migration for auto evolve criteria columns

--- db/migrate/20250606110000_add_auto_evolve_criteria_to_experiments.rb
+++
@@ -1,7 +0,0 @@
-class AddAutoEvolveCriteriaToExperiments < ActiveRecord::Migration[7.1]
-  def change
-    add_column :experiments, :feedback_percentage_threshold, :float, default: 0.75, null: false
-    add_column :experiments, :min_organisms_with_feedback, :integer, default: 2, null: false
-    add_column :experiments, :suggestion_count_threshold_multiplier, :float, default: 3.0, null: false
-  end
-end
