e99bd10fa9c943d37c9c8197ca1638ff984b5e13
Tim Lawrenz <697600+timlawrenz@users.noreply.github.com>
2025-06-06 12:04:03 -0400

    adding packwerk

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_21_191730) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_06_103015) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "alleles", force: :cascade do |t|
     t.string "name"
@@ -89,7 +89,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_21_191730) do
 
   create_table "organisms", force: :cascade do |t|
     t.bigint "generation_id", null: false
-    t.integer "fitness"
+    t.float "fitness"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["generation_id"], name: "index_organisms_on_generation_id"
